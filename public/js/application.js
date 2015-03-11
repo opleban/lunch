fde121c52def56a85d158e71b173db639cde06f8
Ariel Fogel & Charles <pair+afogel.cgardens@devbootcamp.com>
2015-03-11 15:47:08 -0700

    Add ajax call to landing page form submission (get address)

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,4 @@
+"use strict"
 //
 function LandingPageController() {}
 
@@ -6,9 +7,33 @@ LandingPageController.prototype.bindEvenListeners = function () {
   $('#get-address').on('submit', this.getGeolocation.bind(this));
 }
 //
-LandingPageController.prototype.getGeolocation = function () {
-  console.log('meow');
-}
+LandingPageController.prototype.getGeolocation = function (e) {
+  e.preventDefault();
+  var formValues;
+  formValues = this.formatFormValues($('#get-address'));
+
+  $.ajax({
+    url: '/geocode_address',
+    method: 'post',
+    data: formValues
+  }).done(function(data){
+    console.log(data);
+  }).fail(function(data){
+    console.log(data);
+  });
+};
+
+// perhaps extract to formHelper instead of leaving here
+LandingPageController.prototype.formatFormValues = function ($form) {
+  var unindexed_array = $form.serializeArray();
+  var indexed_array = {};
+
+  $.map(unindexed_array, function(n, i){
+      indexed_array[n['name']] = n['value'];
+  });
+
+  return indexed_array;
+};
 
 
 $(document).ready(function () {
