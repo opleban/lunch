5d4d7ec0bf58e0b91ec8c891dce766d053aefa15
Ariel Fogel & Charles <pair+afogel.cgardens@devbootcamp.com>
2015-03-11 15:28:24 -0700

    Create landingPageController in application.js -modify form tag to remove method and action, include id -add listener to form submission

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,17 @@
-$(document).ready(function() {
-	
+//
+function LandingPageController() {}
+
+//
+LandingPageController.prototype.bindEvenListeners = function () {
+  $('#get-address').on('submit', this.getGeolocation.bind(this));
+}
+//
+LandingPageController.prototype.getGeolocation = function () {
+  console.log('meow');
+}
 
 
-});
+$(document).ready(function () {
+  var landingPageController = new LandingPageController();
+  landingPageController.bindEvenListeners();
+})
