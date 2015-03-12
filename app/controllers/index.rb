84d23e4fe94f06eddd4ab409c023bdcd64a69033
Ariel Fogel & Charles <pair+afogel.cgardens@devbootcamp.com>
2015-03-11 17:59:08 -0700

    Extract geolocation logic to apihelper, add error handling

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,7 +6,12 @@ end
 post '/geocode_address' do
   formatted_query_params = params[:formattedAddress]
   lat_long = get_geolocation(formatted_query_params)
-  p lat_long
+  if lat_long
+    get_restaurants(lat_long)
+  else
+    status 400
+    return error_message = {message: 'Please enter a valid address'}.to_json
+  end
   # hit api
   # evaluate response
   # hit new api or return response
--- app/helpers/apiHelper.rb
+++ app/helpers/apiHelper.rb
@@ -1,3 +1,13 @@
 helpers do
-
+  def get_geolocation(formatted_query_params)
+    response = HTTParty.get("http://geocoding.geo.census.gov/geocoder/locations/address?format=json&benchmark=9&#{formatted_query_params}")
+    if response.code == 200
+      parsed_response = JSON.parse(response.body)
+      # rename keys to be more descriptive:
+      x_y_hash = parsed_response['result']['addressMatches'].first['coordinates']
+      lat_long_hash = {long: x_y_hash['x'], lat: x_y_hash['y']}
+    else
+      return nil
+    end
+  end
 end
